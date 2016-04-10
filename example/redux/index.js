412bd5833fce0be555e65acb4e39ed2962ed7a01
BearJ <bearverygood@126.com>
2016-04-10 22:21:13 +0800

    add router

--- example/redux/index.js
+++ example/redux/index.js
@@ -1,15 +1,49 @@
-import React from "react"
-import { render } from "react-dom"
-import { createStore } from "redux"
-import { Provider } from "react-redux"
-import App from "./containers/App"
-import todoApp from "./reducers/index"
-import todoStore from "./store/index"
+import React, { Component } from "react";
+import { Router, Route, Link, browserHistory } from "react-router";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import todoApp from "./reducers/index";
+import todoStore from "./store/index";
 
-let rootElement = document.getElementById("root");
-render(
-    <Provider store={todoStore}>
-        <App />
-    </Provider>,
-    rootElement
-);
+class ReduxApp extends Component{
+    render() {
+        return (
+            <Provider store={todoStore}>
+                <App />
+            </Provider>
+        )
+    }
+}
+
+class About extends Component{
+    render() {
+        return (
+            <h2>My name is Bear.</h2>
+        )
+    }
+}
+
+class IndexApp extends Component{
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><Link to="/app">app</Link></li>
+                    <li><Link to="/about">about</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+render((
+    <Router>
+        <Route path="/" component={IndexApp}>
+            <Route path="app" component={ReduxApp} />
+            <Route path="about" component={About} />
+        </Route>
+    </Router>
+), document.getElementById("root"));
